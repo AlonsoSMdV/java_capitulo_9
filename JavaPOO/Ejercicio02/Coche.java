8b3e0443c3de423551d46544cfa6b1acfb6ce0dc
AlonsoSMdV <asanmor011004@gmail.com>
2023-01-12 18:56:40 +0100

    New Commit

---
+++ JavaPOO/Ejercicio02/Coche.java
@@ -0,0 +1,12 @@
+package JavaPOO.Ejercicio02;
+
+public class Coche extends Vehiculo {
+    private int cilindrada; // cilindrada(cm3)
+    public Coche(int c) {
+        super();
+        this.cilindrada = c;
+    }
+    public void quemaRueda() {
+        System.out.println("Fffshhhhhhhhhhh");
+    }
+}
