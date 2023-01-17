0372139adb6121ce9dc0efe6898fbf0a62f964c2
AlonsoSMdV <asanmor011004@gmail.com>
2023-01-17 17:31:38 +0100

    New Commit

---
+++ JavaPOO/Ejercicio07/Zona.java
@@ -0,0 +1,25 @@
+package JavaPOO.Ejercicio07;
+
+public class Zona {
+    private int entradasPorVender;
+    public Zona(int n){
+        entradasPorVender = n;
+    }
+    public int getEntradasPorVender() {
+        return entradasPorVender;
+    }
+    /**
+    * Vende un número de entradas.
+    */
+    public void vender(int n) {
+        if (this.entradasPorVender == 0) {
+            System.out.println("Lo siento, las entradas para esa zona están agotadas.");
+        } else if (this.entradasPorVender < n) {
+        System.out.println("Sólo me quedan " + this.entradasPorVender + " entradas para esa zona.");
+        }
+        if (this.entradasPorVender >= n) {
+            entradasPorVender -= n;
+            System.out.println("Aquí tiene sus " + n + " entradas, gracias.");
+        }
+    }
+}    
