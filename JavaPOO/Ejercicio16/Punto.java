414d0a7b56584ef760c106ae335c3d4c703ed465
AlonsoSMdV <asanmor011004@gmail.com>
2023-01-26 17:31:26 +0100

    New Commit

---
+++ JavaPOO/Ejercicio16/Punto.java
@@ -0,0 +1,13 @@
+package JavaPOO.Ejercicio16;
+public class Punto {
+    private double x;
+    private double y;
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+} 
